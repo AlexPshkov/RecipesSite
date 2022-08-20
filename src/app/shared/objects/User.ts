1d132b2af2a639f64906d976984de2336b54790c
AlexPshkov <alexpshkov@gmail.com>
2022-08-20 16:43:59 +0300

    Add user password to DTO and profile page

--- src/app/shared/objects/User.ts
+++ src/app/shared/objects/User.ts
@@ -1,13 +1,15 @@
 export class User {
   id: string | null;
-  userName: string | null;
+  userName: string;
+  password: string;
   login: string;
   description: string;
   role: string;
 
-  constructor(id: string, userName: string, login: string, description: string, role: string) {
+  constructor(id: string, userName: string, password: string, login: string, description: string, role: string) {
     this.id = id;
     this.userName = userName;
+    this.password = password;
     this.login = login;
     this.description = description;
     this.role = role;
