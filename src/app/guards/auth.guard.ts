a1328afe6dcfd596d328e16e34398aea83039d7b
AlexPshkov <alexpshkov@gmail.com>
2022-08-22 17:16:44 +0300

    Add new page. (AddRecipe)

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -18,10 +18,11 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (!this.authService.isAuthenticated()) this.router.navigate([''])
-      .then(() => this.dialog.open(RegisterDialogComponent));
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['']).then(() => this.dialog.open(RegisterDialogComponent));
+      return false;
+    }
     return true;
-
   }
 
 }
